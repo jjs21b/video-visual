400d6bf870b24258fedc98376279cd257f5705fd
jjs21b <jschwartz1125@gmail.com>
2024-03-29 18:35:31 -0400

    Reinstalled Tailwind and Recharts, added GameReleasesChart component

--- my-video-visual/src/App.js
+++ my-video-visual/src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
 import './App.css';
-import GamesList from './GamesList'; // Importing your GamesList component
-
+import React from 'react';
 function App() {
   return (
     <div className="App">
-      <GamesList /> // Using your GamesList component
+      <GameReleasesChart year={2021} />
     </div>
   );
 }
