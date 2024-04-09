3265e4d54a5002ba07e9be700be9ec17aa75630b
jjs21b <jschwartz1125@gmail.com>
2024-04-09 13:53:30 -0400

    removed sidebar from detailed view

--- my-video-visual/src/App.js
+++ my-video-visual/src/App.js
@@ -1,17 +1,32 @@
-import './App.css';
-// App.js
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Sidebar from './sidebar';
-import GamesDisplay from './ResultsDisplay'; // Adjust the import path as necessary
+import GamesDisplay from './ResultsDisplay';
+import GameDetails from './GameDetails';
 
 const App = () => {
   const [games, setGames] = useState([]);
 
+  const AppContent = () => {
+    const location = useLocation(); // Now it's used in a child component of Router
+
+    const showSidebar = !location.pathname.startsWith('/game/');
+
+    return (
+      <div className="app flex">
+        {showSidebar && <Sidebar setGames={setGames} />}
+        <Routes>
+          <Route path="/" element={<GamesDisplay games={games} />} />
+          <Route path="/game/:id" element={<GameDetails />} />
+        </Routes>
+      </div>
+    );
+  };
+
   return (
-    <div className="app flex">
-      <Sidebar setGames={setGames} />
-      <GamesDisplay games={games} />
-    </div>
+    <Router basename='/video-visual'>
+      <AppContent />
+    </Router>
   );
 };
 
